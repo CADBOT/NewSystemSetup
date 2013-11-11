a4ff6273a3c025749e7fe3513bddc3136620e177
cadbot <charliec364@gmail.com>
2013-11-11 00:03:21 +0000

    added graphic section

--- bootstrap.fish
+++ bootstrap.fish
@@ -2,6 +2,10 @@
 # Install homesick to setup dotfiles
 # TODO: Add options for some of this stuff
 # TODO: Add yes to all options for apt-get install stuff
+# See if graphical programs should be installed
+echo 'Should graphical programs be installed? yes or no?'
+# TODO: Add input validation loop
+read installGraphical
 gem install homesick
 homesick clone cadbot/dotfiles
 homesick symlink dotfiles
@@ -23,5 +27,19 @@ sudo pip install virtualenvwrapper
 # Install Java/Scala stuff
 sudo apt-get install openjdk-7-jdk
 curl -O http://www.scala-lang.org/files/archive/scala-2.10.3.deb
-dpkg -i scala-2.10.3.deb
+sudo dpkg -i scala-2.10.3.deb
 rm scala-2.10.3.deb
+
+# TODO: install eclipse and eclipse plugins
+
+# Extra command line stuff
+sudo apt-get install cowsay
+sudo apt-get install xclip
+
+# GUI programs
+#Install steam
+if [ $installGraphical = 'yes' ]
+  curl -O http://media.steampowered.com/client/installer/steam.deb
+  sudo dpkg -i steam.deb
+  rm steam.deb
+end
