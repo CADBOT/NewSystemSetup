9335bafec82396109ee05fe31be4495940c9b1da
cadbot <charliec364@gmail.com>
2013-11-11 02:16:10 +0000

    Added echo commands and an apt-get -f

--- bootstrap.fish
+++ bootstrap.fish
@@ -6,35 +6,55 @@
 echo 'Should graphical programs be installed? yes or no?'
 # TODO: Add input validation loop
 read installGraphical
+echo 'sudo gem install homesick'
 sudo gem install homesick
+echo 'homesick clone CADBOT/dotfiles'
 homesick clone CADBOT/dotfiles
+echo 'homesick symlink dotfiles'
 homesick symlink dotfiles
 
 # Install programming language stuff
 
 # Install Ruby stuff 
 # Install and configure rvm
+echo 'curl -L get.rvm.io | bash -s stable'
 curl -L get.rvm.io | bash -s stable
 # RVM fish functions
+echo 'curl --create-dirs -o ~/.config/fish/functions/rvm.fish https://raw.github.com/lunks/fish-nuggets/master/functions/rvm.fish'
 curl --create-dirs -o ~/.config/fish/functions/rvm.fish https://raw.github.com/lunks/fish-nuggets/master/functions/rvm.fish
 # Install and make default Ruby 2.0.0
+echo 'rvm install 2.0.0'
 rvm install 2.0.0
+echo 'rvm use --default 2.0.0'
 rvm use --default 2.0.0
 # Install python stuff
+echo 'sudo apt-get install -y python-pip'
 sudo apt-get install -y python-pip
+echo 'sudo pip install -y virtualenv'
 sudo pip install -y virtualenv
+echo 'sudo pip install -y virtualenvwrapper'
 sudo pip install -y virtualenvwrapper
 # Install Java/Scala stuff
+echo 'sudo apt-get install -y openjdk-7-jdk'
 sudo apt-get install -y openjdk-7-jdk
+echo 'curl -O http://www.scala-lang.org/files/archive/scala-2.10.3.deb'
 curl -O http://www.scala-lang.org/files/archive/scala-2.10.3.deb
+echo 'sudo dpkg -i scala-2.10.3.deb'
 sudo dpkg -i scala-2.10.3.deb
+echo 'rm scala-2.10.3.deb'
 rm scala-2.10.3.deb
 
 # TODO: install eclipse and eclipse plugins
 
 # Extra command line stuff
+echo 'sudo apt-get install -y cowsay'
 sudo apt-get install -y cowsay
+echo 'sudo apt-get install -y xclip'
 sudo apt-get install -y xclip
+
+# To wrap up
+echo 'sudo apt-get -f install'
+sudo apt-get -f install
 
 # GUI programs
 #Install steam
