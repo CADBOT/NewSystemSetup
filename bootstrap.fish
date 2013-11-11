c7ec390e881551be8b854dbb2b97720d86e20180
cadbot <charliec364@gmail.com>
2013-11-11 01:38:40 +0000

    Fixed Ruby installer

--- bootstrap.fish
+++ bootstrap.fish
@@ -14,18 +14,18 @@ homesick symlink dotfiles
 
 # Install Ruby stuff 
 # Install and configure rvm
-\curl -L https://get.rvm.io | bash
+curl -L get.rvm.io | bash -s stable
 # RVM fish functions
 curl --create-dirs -o ~/.config/fish/functions/rvm.fish https://raw.github.com/lunks/fish-nuggets/master/functions/rvm.fish
 # Install and make default Ruby 2.0.0
 rvm install 2.0.0
 rvm use --default 2.0.0
 # Install python stuff
-sudo apt-get install python-pip
-sudo pip install virtualenv
-sudo pip install virtualenvwrapper
+sudo apt-get install -y python-pip
+sudo pip install -y virtualenv
+sudo pip install -y virtualenvwrapper
 # Install Java/Scala stuff
-sudo apt-get install openjdk-7-jdk
+sudo apt-get install -y openjdk-7-jdk
 curl -O http://www.scala-lang.org/files/archive/scala-2.10.3.deb
 sudo dpkg -i scala-2.10.3.deb
 rm scala-2.10.3.deb
@@ -33,8 +33,8 @@ rm scala-2.10.3.deb
 # TODO: install eclipse and eclipse plugins
 
 # Extra command line stuff
-sudo apt-get install cowsay
-sudo apt-get install xclip
+sudo apt-get install -y cowsay
+sudo apt-get install -y xclip
 
 # GUI programs
 #Install steam
